3e578b4b0959108b044aca63fba6b89fedf8f21a
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2024-09-26 10:38:21 -0600

    Portales configurables
    Corregido problema que vinculaba al mismo portal para conservar inercia.
    Creado Prefab para portales configurables.
    Corregido error que permitía un portal sin portal de salida.
    Añadida funcionalidad para perder, conservar o redirigir la inercia del jugador con un factor.

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,21 +1,26 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
-{
-    [Header("Portal Configuration")]
-    [Tooltip("Define el portal de salida")]
-    public Transform portalSalida;
-
+public class Portal : MonoBehaviour {
+    [Header("Configuración del Portal")]
     [Tooltip("Define si el portal está activo o no")]
     public bool activo = true;
 
-    [Tooltip("Define si se conserva la inercia del jugador tras teletransportarse")]
-    public bool conservarInercia = false;
+    [Tooltip("Arrastra un portal de salida")]
+    public Portal portalSalida;
+    [Tooltip("Añade un multiplicador de lanzamiento (redirección)")]
+    public float multiplicadorInercia = 1.0f;
+    [Tooltip("Añade un ángulo de lanzamiento (redirección)")]
+    [Range(0, 359)]
+    public int angulo = 0;
+
+    [Tooltip("Define si se conserva la inercia tras teletransportarse")]
+    public bool conservarInercia = true;
+    [Tooltip("Elige si debe redirigir tras teletransportar")]
+    public bool redirigir = false;
 
     [Tooltip("Tiempo de espera (en segundos) para reutilizar el portal")]
-    public float tiempoEspera = 2f;
+    public float tiempoEspera = 1.0f;
     private bool enEspera = false;
 
     private Rigidbody2D rbJugador;
@@ -31,6 +36,9 @@ public class Portal : MonoBehaviour
     }
     
     void FixedUpdate() {
+        //Bloqueamos si no hay conexión
+        if (!portalSalida) activo = false;
+
         //Si hay un jugador, teletransportarlo
         if (rbJugador) {
             TeletransportarJugador();
@@ -40,16 +48,32 @@ public class Portal : MonoBehaviour
 
     private void TeletransportarJugador() {
         //Mover al jugador al nuevo portal de salida
-        rbJugador.position = portalSalida.position;
+        rbJugador.position = portalSalida.transform.position;
 
         //Reiniciar la velocidad si no quieres conservar la inercia
-        if (!conservarInercia) {
+        if (portalSalida.conservarInercia) {
+            if (portalSalida.redirigir) {
+                //Redirigimos al jugador
+                rbJugador.velocity = AnguloADireccion(portalSalida.angulo) * rbJugador.velocity.magnitude * multiplicadorInercia;
+            }
+        } else {
+            //Reseteamos la velocidad del jugador
             rbJugador.velocity = Vector2.zero;
         }
 
         //Poner ambos portales en espera
         StartCoroutine(TiempoDeEspera(tiempoEspera));
         StartCoroutine(portalSalida.GetComponent<Portal>().TiempoDeEspera(tiempoEspera));
+    }
+
+    private Vector2 AnguloADireccion(float angulo) {
+        //Convertimos el angulo a coordenadas
+        float radianes = (angulo + 90) * Mathf.Deg2Rad;
+
+        //Obtenemos las coordenadas
+        float x = Mathf.Cos(radianes);
+        float y = Mathf.Sin(radianes);
+        return new Vector2(x, y).normalized;
     }
 
     public IEnumerator TiempoDeEspera(float time) {
@@ -62,12 +86,18 @@ public class Portal : MonoBehaviour
         //Desactivar el tiempo de espera del portal
         enEspera = false;
     }
-
+    
     private void OnDrawGizmos() {
         //Visualizar en el Editor las conexiones entre portales
         if (portalSalida != null) {
             Gizmos.color = Color.cyan;
-            Gizmos.DrawLine(transform.position, portalSalida.position);
+            Gizmos.DrawLine(transform.position, portalSalida.transform.position);
+        }
+
+        //Visualizamos la dirección de salida del portal
+        if (conservarInercia && redirigir) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, (Vector2)transform.position + (AnguloADireccion(angulo) * multiplicadorInercia));
         }
     }
 }
