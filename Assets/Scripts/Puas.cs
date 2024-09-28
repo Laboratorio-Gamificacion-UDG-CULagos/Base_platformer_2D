41c3a4d51b84ca14d5bea0b7639d91f30f8a03c9
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2024-09-27 22:37:40 -0600

    Púas
    Añadido sprite FullRect para púas.
    Configurado prefab de púas y adaptables a tamaños y largos con la posibilidad de deshabilitarse.

--- Assets/Scripts/Puas.cs
+++ Assets/Scripts/Puas.cs
@@ -1,9 +1,4 @@
 using UnityEngine;
 
-public class Puas : MonoBehaviour {
-    private void OnTriggerEnter2D(Collider2D colisionado) {
-        if (colisionado.gameObject.name == "Jugador") {
-            colisionado.gameObject.GetComponent<Personaje>().Herir();
-        }
-    }
+public class Puas : Interactuable {
 }
