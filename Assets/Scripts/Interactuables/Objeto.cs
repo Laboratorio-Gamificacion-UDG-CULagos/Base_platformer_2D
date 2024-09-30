cb7e6c6870d7a25065e78d7b9df74919ced9c692
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2024-09-29 21:11:09 -0600

    Objetos recolectables
    Añadidas capas Moneda, Estrella, Joya.
    Añadidos objetos prefab Estrella, Moneda y Joya, estos últimos con sus respectivas variantes.
    Configurados prefabs Estrella, Moneda y Joya con valores cuantitativos de ejemplo.
    Reasignamos la carpeta para guardar Objetos dentro de los interactuables.

---
+++ Assets/Scripts/Interactuables/Objeto.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Objeto : Interactuable {
+    [Header("Configuración del objeto")]
+    [Tooltip("Elige un valor cuantitativo")]
+    [SerializeField, Min(0)] private int valor = 1;
+    
+    private void OnTriggerEnter2D(Collider2D colisionado) {
+        //Detecta colisiones con el jugador
+        if(colisionado.CompareTag("Jugador")) {
+            //PENDIENTE AÑADIR DESENCADENAMIENTO DE ACCIONES UI
+        }
+
+        //Destruye la llave milisegundos despues
+        Destroy(gameObject, 0.05f);
+    }
+}
