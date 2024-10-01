2e95d93cb63f7f3d8d529ee41b6b7a1e1c4fdec2
FerCamarena <fernando.camarena6817@alumnos.udg.mx>
2024-10-01 11:26:04 -0600

    Heridores
    Añadida capa para manejar heridores.
    Modificada capa 'púas' para ahora llamarse 'trampas' y manejar objetos peligrosos con comportamientos.
    Añadido objeto prefab Cactus.
    Configurado comportamiento para Cactus, permitiendo ser un peligro estático, heridor.
    Modificado comportamiento de púas para funcionar heredados desde heridores, permitiendo ahorrar código y simplificar comportamientos.

---
+++ Assets/Scripts/Interactuables/Heridor.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class Heridor : Interactuable {
+    [Header("Configuración del heridor")]
+    [Tooltip("Asigna un tiempo de espera del cactus")]
+    [SerializeField, Min(0)] protected float tiempoEspera = 0.5f;
+    [Space(5)]
+    [Tooltip("Elige el valor de daño al contacto")]
+    [SerializeField, Min(0)] protected int golpe = 1;
+    [Tooltip("Permite lanzar al jugador de regreso")]
+    [SerializeField] protected bool repeler;
+    [Tooltip("Agrega un multiplicador de repulsión")]
+    [SerializeField, Min(0)] protected float factorRepulsion = 1.0f;
+    [Tooltip("Anula el movimiento del jugador")]
+    [SerializeField] protected bool detener;
+
+    [Space(20)]
+    [Header("DEV (Variables de control)")]
+    [SerializeField] protected Transform sprite;
+    [SerializeField] protected bool enEspera = false;
+    [SerializeField] protected Vector2 direccion;
+
+    private void OnTriggerEnter2D(Collider2D colisionado) {
+        //Detectamos la colisión con el jugador
+        if (colisionado.CompareTag("Jugador") && !enEspera && activo) {
+            //Actualizamos su vida
+            colisionado.GetComponent<Personaje>().vida -= golpe;
+
+            //Checamos si habilita repeler
+            if (repeler) {
+                Rigidbody2D rbJugador = colisionado.GetComponent<Rigidbody2D>();
+                Vector2 nuevaVelocidad = new Vector2(-rbJugador.velocity.x, -rbJugador.velocity.y / 5) * factorRepulsion;
+                rbJugador.velocity = nuevaVelocidad;
+                rbJugador.AddForce(Vector2.up * 500 * factorRepulsion, ForceMode2D.Impulse);
+            } else if (detener) {
+                colisionado.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
+
+            //Establecemos en espera de las púas
+            StartCoroutine(TiempoDeEspera(tiempoEspera));
+        }
+    }
+
+    protected virtual IEnumerator TiempoDeEspera(float espera) {
+        //Activar el tiempo de espera del resorte
+        enEspera = true;
+
+        //Espera el tiempo especificado
+        yield return new WaitForSeconds(espera);
+
+        //Desactivar el tiempo de espera del resorte
+        enEspera = false;
+    }
+}
---
+++ Assets/Scripts/Interactuables/Puas.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+public class Puas : Heridor {
+    [Header("Configuración de las púas")]
+    [Tooltip("Elige si respeta un angulo personalizado")]
+    [SerializeField] private bool anguloPersonalizado;
+    [Tooltip("Asigna un ángulo si está activa la personalización")]
+    [SerializeField, Range(0, 359)] private int anguloFuerza;
+
+    private void OnValidate() {
+        ActualizarDireccion();
+    }
+    
+    protected void Start() {
+        ActualizarDireccion();
+    }
+
+    private void ActualizarDireccion() {
+        //Obtener la rotación Z del objeto y ajustar el ángulo de lanzamiento
+        if(!anguloPersonalizado) anguloFuerza = (int)transform.eulerAngles.z % 359;
+
+        //Calcula la dirección de la normal del resorte según el ángulo
+        float radianes = (anguloFuerza + 90) * Mathf.Deg2Rad;
+        direccion = new Vector2(Mathf.Cos(radianes), Mathf.Sin(radianes));
+    }
+
+    protected override IEnumerator TiempoDeEspera(float espera) {
+        //Activar el tiempo de espera del resorte
+        enEspera = true;
+
+        //Baja su sprite si está asignado
+        if (sprite && sprite.localPosition.y <= 0.0f) sprite.position += (Vector3)direccion * 0.4f;
+
+        //Espera el tiempo especificado
+        yield return new WaitForSeconds(espera);
+
+        //Desactivar el tiempo de espera del resorte
+        enEspera = false;
+
+        //Regresa a la posición original el sprite si existe
+        while (sprite && sprite.localPosition.y > 0.0f) {
+            sprite.position = Vector2.MoveTowards(sprite.position, transform.position, 0.01f);
+            yield return null;
+        }
+    }
+
+    private void OnDrawGizmos() {
+        //Visualizamos la dirección de salida de las púas
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine((Vector2)transform.position, (Vector2)transform.position + direccion / 2);
+    }
+}
